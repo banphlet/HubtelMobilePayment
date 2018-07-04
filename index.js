aee34dfda89699e9516f9cc3d47a941299f678e9
Nana Kwame Zoe <annor_theophilus@hotmail.com>
2018-07-04 19:48:40 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var RefundMoney = require('./lib/refundmobile');
 var onlinecheckout = require('./lib/onlinecheckout');
 var checkstatus = require('./lib/checkstatus');
 /**
- * Set up mpower authentication
+ * Set up Hubtel authentication
  * @param {object} data
  */
 function HubtelMobilePayment(data){
     if(!data.secretid ||  !data.clientid || !data.merchantaccnumber){
-        throw ('hubtelmobile Authentication failed')
+        throw ('required parameters not provided')
     }
   this.config = {}
   this.config['clientid'] = data && data.clientid || process.env.clientid;
